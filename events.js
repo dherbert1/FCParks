6ba21bd0e93448f8d1053e54163e4e2e1936d657
tyler1 <168108668+tyler123235234@users.noreply.github.com>
2025-03-05 01:20:29 +0000

    yep

--- events.js
+++ events.js
@@ -24,3 +24,40 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
+let elem = document.querySelector('.photos__slider');
+function handlePhotos() {
+  let flkty = new Flickity(elem, {
+    wrapAround: true,
+    autoplay: 5,
+    fullscreen: true,
+    pageDots: false,
+    prevNextButtons: true,
+    on: [ 
+      {
+        ready: function () {
+          heightCard();
+        },
+      }
+    ],
+  });
+};
+
+function heightCard() {
+  let slides = document.querySelectorAll(".photos__slider-item");
+  let maxHeight = 0;
+  slides.forEach(function (slide) {
+    let height = slide.offsetHeight;
+    if (height > maxHeight) {
+      maxHeight = height;
+    }
+  });
+  slides.forEach(function (slide) {
+    slide.style.height = `${maxHeight}px`;
+  });
+}
+window.addEventListener("load", function () {
+  handlePhotos();
+});
+window.addEventListener("resize", function () {
+  handlePhotos();
+});
