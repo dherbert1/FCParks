1bd310b795dc44ba6bf2b3913da23eff5d20bdc4
tyler1 <168108668+tyler123235234@users.noreply.github.com>
2025-03-14 17:35:39 +0000

    s

--- index.js
+++ index.js
@@ -81,7 +81,29 @@ function handlePhotos() {
 
   lazyLoadImages(); // Initialize lazy loading for images
 };
-
+function handleServices() {
+  let flkty = new Flickity("photos__slider-item", {
+    groupCells: 2,
+    wrapAround: true,
+    pageDots: true,
+    prevNextButtons: true,
+    responsive: [
+      {
+        breakpoint: 991.98,
+        settings: {
+          groupCells: 1,
+        },
+      },
+    ],
+    on: [ 
+      {
+        ready: function () {
+          heightCard();
+        },
+      }
+    ],
+  });
+};
 function heightCard() {
   let slides = document.querySelectorAll(".photos__slider-item");
   let maxHeight = 0;
